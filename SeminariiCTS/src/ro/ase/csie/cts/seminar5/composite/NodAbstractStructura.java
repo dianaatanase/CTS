e7dbcd5cb1daee306e9428c89c257e9b466fd565
Diana <dianabianca0923@gmail.com>
2021-04-16 10:32:19 +0300

    Implementare Composite

---
+++ SeminariiCTS/src/ro/ase/csie/cts/seminar5/composite/NodAbstractStructura.java
@@ -0,0 +1,8 @@
+package ro.ase.csie.cts.seminar5.composite;
+
+public abstract class NodAbstractStructura implements InterfataCaractereNPC {
+	
+	public abstract void adaugaNod(NodAbstractStructura nod);
+	public abstract void stergeNod(NodAbstractStructura nod);
+	public abstract NodAbstractStructura getNod(int index);
+}
