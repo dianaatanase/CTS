565ec46c96d91ecf57da65c4e254ed5d626f0fcc
Diana <dianabianca0923@gmail.com>
2021-04-21 14:40:22 +0300

    Implementare Proxy

---
+++ SeminariiCTS/src/ro/ase/csie/cts/seminar9/dp/proxy/InterfataModulLogin.java
@@ -0,0 +1,6 @@
+package ro.ase.csie.cts.seminar9.dp.proxy;
+
+public interface InterfataModulLogin {
+	public boolean login(String username, String pass);
+	public boolean verificaStatusServer();
+}
