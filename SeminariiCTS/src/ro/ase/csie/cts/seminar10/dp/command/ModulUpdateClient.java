73d2e3a1b2da71454a6dac16730c2bdcd264b426
Diana <dianabianca0923@gmail.com>
2021-05-05 15:25:46 +0300

    Implementare command 1

---
+++ SeminariiCTS/src/ro/ase/csie/cts/seminar10/dp/command/ModulUpdateClient.java
@@ -0,0 +1,11 @@
+package ro.ase.csie.cts.seminar10.dp.command;
+
+public class ModulUpdateClient implements InterfataModulJoc{
+
+	@Override
+	public void executaActiune(String numeActiune) {
+		System.out.println("Update client joc. Detalii: " + numeActiune);
+		
+	}
+
+}
---
+++ SeminariiCTS/src/ro/ase/csie/cts/seminar10/dp/command/TaskToDo.java
@@ -0,0 +1,11 @@
+package ro.ase.csie.cts.seminar10.dp.command;
+
+public class TaskToDo extends TaskAsincron{
+
+	@Override
+	public void executaTask() {
+		
+		
+	}
+
+}
