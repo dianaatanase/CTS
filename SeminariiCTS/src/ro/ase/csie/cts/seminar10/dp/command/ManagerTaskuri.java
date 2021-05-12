0c30a52127286569efd0b3a5fb97d167f566263b
Diana <dianabianca0923@gmail.com>
2021-05-12 14:24:14 +0300

    Implementare manager command

---
+++ SeminariiCTS/src/ro/ase/csie/cts/seminar10/dp/command/ManagerTaskuri.java
@@ -0,0 +1,21 @@
+package ro.ase.csie.cts.seminar10.dp.command;
+
+import java.util.ArrayList;
+
+public class ManagerTaskuri {
+
+	ArrayList<TaskAsincron> taskuri = new ArrayList<>();
+	
+	public void adaugaTask(TaskAsincron task) {
+		taskuri.add(task);
+	}
+	
+	
+	public void executaTask() {
+		if(taskuri.size() > 0) {
+			TaskAsincron task = taskuri.get(0);
+			task.executaTask();
+			taskuri.remove(0);
+		}
+	}
+}
