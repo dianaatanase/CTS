7c178096ee44f561da5ee1672a899857f4f325f9
Diana <dianabianca0923@gmail.com>
2021-05-19 15:28:25 +0300

    Testare Unitara - code coverage

--- Seminar12 -Testare Unitara/src/ro/ase/csie/cts/testare/teste/TestOperatiiMatematice.java
+++ Seminar12 -Testare Unitara/src/ro/ase/csie/cts/testare/teste/TestOperatiiMatematice.java
@@ -8,33 +8,40 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import ro.ase.csie.cts.testare.exceptii.ExceptieImpartireLaZero;
 import ro.ase.csie.cts.testare.modele.OperatiiMatematice;
 
 public class TestOperatiiMatematice {
 
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
+		System.out.println("Salut!");
 	}
 
 	@AfterClass
 	public static void tearDownAfterClass() throws Exception {
+		System.out.println("La revedere!");
 	}
 
 	@Before
 	public void setUp() throws Exception {
+		System.out.println("Pregatire test..");
 	}
 
 	@After
 	public void tearDown() throws Exception {
+		System.out.println("Stergere resurse folosite.");
 	}
 
 	@Test
 	public void test() {
+		System.out.println("Test de test");
 		fail("Not yet implemented");
 	}
 
 	@Test
 	public void testAdunaValoriPozitive() {
+		System.out.println("Test aduna");
 		int a =5;
 		int b=5;
 		int rezultatAsteptat =10;
@@ -42,4 +49,21 @@ public class TestOperatiiMatematice {
 		
 		assertEquals("Suma cu numere pozitive", rezultatAsteptat, rezultatCalculat);
 	}
+	
+	@Test
+	public void testImpartireValoriPozitive() {
+		int a=1;
+		int b=2;
+		double rezultatAsteptat = 0.5;
+		double rezultatPrimit = 0;
+		
+		try {
+			rezultatPrimit = OperatiiMatematice.impartire(a, b);
+		} catch (ExceptieImpartireLaZero e) {
+			fail("Am primit exceptie, desi nu trebuia");
+		}
+		
+		assertEquals("Impartire 1/2",rezultatAsteptat, rezultatPrimit, 0);
+	}
+	
 }
