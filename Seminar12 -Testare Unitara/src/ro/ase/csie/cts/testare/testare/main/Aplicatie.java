a07139182230f81c1296cbad084f9bf850d1be33
Diana <dianabianca0923@gmail.com>
2021-05-19 14:42:10 +0300

    Implementare Testare Unitara 1

---
+++ Seminar12 -Testare Unitara/src/ro/ase/csie/cts/testare/testare/main/Aplicatie.java
@@ -0,0 +1,24 @@
+package ro.ase.csie.cts.testare.testare.main;
+
+import ro.ase.csie.cts.testare.modele.OperatiiMatematice;
+
+public class Aplicatie {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello! Aplicatia a pornit.");
+		
+//		int a =5;
+//		int b=5;
+//		int rezultatAsteptat =10;
+//		int rezultatCalculat = OperatiiMatematice.aduna(a, b);
+//		
+//		if(rezultatAsteptat == rezultatCalculat) {
+//			System.out.println("Metoda aduna este ok");
+//		}else {
+//			System.out.println("Mai trebuie niste cursuri de mate");
+//		}
+
+	}
+
+}
